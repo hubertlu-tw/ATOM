77f581052bfe5e531598a9ec67e943b522742348
ZhangLirong <lirzhang@amd.com>
2026-01-26 21:43:35 +0800

    open fused shared_expert in dp (#163)
    Co-authored-by: Lingpeng Jin <103567126+valarLip@users.noreply.github.com>

--- atom/model_ops/topK.py
+++ atom/model_ops/topK.py
@@ -15,7 +15,7 @@ from aiter.jit.utils.torch_guard import torch_compile_guard
 def is_rocm_aiter_fusion_shared_expert_enabled() -> bool:
     config = get_current_atom_config()
     dp_size = config.parallel_config.data_parallel_size
-    if dp_size > 1 and _has_module("mori"):
+    if dp_size > 1 and _has_module("mori") and config.enable_dp_attention:
         return False
     return True
 
